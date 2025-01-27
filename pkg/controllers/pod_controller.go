dda13a7893d3c44603d0d13c1558afe7b777a72d
Edwinhr716 <Edandres249@gmail.com>
2025-01-27 16:49:24 +0000

    handled potential nil dereference

--- pkg/controllers/pod_controller.go
+++ pkg/controllers/pod_controller.go
@@ -21,6 +21,7 @@ import (
 	"errors"
 	"fmt"
 	"strconv"
+	"time"
 
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
@@ -126,6 +127,10 @@ func (r *PodReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.R
 	if err != nil {
 		log.Error(err, "Getting lws revisions")
 		return ctrl.Result{}, err
+	}
+	if revision == nil {
+		log.V(2).Info("Revision has not been created yet, requeing reconciler for pod %s", pod.Name)
+		return ctrl.Result{Requeue: true, RequeueAfter: time.Second}, nil
 	}
 	statefulSet, err := constructWorkerStatefulSetApplyConfiguration(pod, leaderWorkerSet, revision)
 	if err != nil {
