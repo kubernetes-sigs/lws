52c15931f7b6403f5e1411e3787252d05985bbf2
Arda Güçlü <aguclu@redhat.com>
2025-02-10 10:25:58 +0300

    Deprecate flags as the recommended path is to pass configuration file (#361)
    * Deprecate some flags that can be passed through --config
    
    * Use GetScaledValueFromIntOrPercent instead of deprecated GetValueFromIntOrPercent
    
    * Move metricserver binding into apply function for better readability
    
    * Add cmd unit test execution in make test

--- cmd/main.go
+++ cmd/main.go
@@ -34,7 +34,6 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 	"sigs.k8s.io/controller-runtime/pkg/metrics/filters"
 	metricsserver "sigs.k8s.io/controller-runtime/pkg/metrics/server"
-
 	configapi "sigs.k8s.io/lws/api/config/v1alpha1"
 	leaderworkersetv1 "sigs.k8s.io/lws/api/leaderworkerset/v1"
 	"sigs.k8s.io/lws/pkg/cert"
@@ -76,28 +75,28 @@ func main() {
 		configFile               string
 	)
 
-	flag.StringVar(&metricsAddr, "metrics-bind-address", ":8443", "The address the metric endpoint binds to.")
-	flag.StringVar(&probeAddr, "health-probe-bind-address", ":8081", "The address the probe endpoint binds to.")
+	flag.StringVar(&metricsAddr, "metrics-bind-address", ":8443", "DEPRECATED(please pass configuration file via --config flag): The address the metric endpoint binds to.")
+	flag.StringVar(&probeAddr, "health-probe-bind-address", ":8081", "DEPRECATED(please pass configuration file via --config flag): The address the probe endpoint binds to.")
 	flag.Float64Var(&qps, "kube-api-qps", 500, "Maximum QPS to use while talking with Kubernetes API")
 	flag.IntVar(&burst, "kube-api-burst", 500, "Maximum burst for throttle while talking with Kubernetes API")
 	flag.BoolVar(&enableLeaderElection, "leader-elect", true,
 		"Enable leader election for controller manager. "+
 			"Enabling this will ensure there is only one active controller manager.")
 	flag.DurationVar(&leaderElectLeaseDuration, "leader-elect-lease-duration", 15*time.Second,
-		"The duration that non-leader candidates will wait after observing a leadership renewal until attempting to acquire "+
+		"DEPRECATED(please pass configuration file via --config flag): The duration that non-leader candidates will wait after observing a leadership renewal until attempting to acquire "+
 			"leadership of a led but unrenewed leader slot. This is effectively the maximum duration that a leader can be stopped"+
 			" before it is replaced by another candidate. This is only applicable if leader election is enabled.")
 	flag.DurationVar(&leaderElectRenewDeadline, "leader-elect-renew-deadline", 10*time.Second,
-		"The interval between attempts by the acting master to renew a leadership slot before it stops leading. This"+
+		"DEPRECATED(please pass configuration file via --config flag): The interval between attempts by the acting master to renew a leadership slot before it stops leading. This"+
 			"must be less than or equal to the lease duration. This is only applicable if leader election is enabled.")
 	flag.DurationVar(&leaderElectRetryPeriod, "leader-elect-retry-period", 2*time.Second,
-		"The duration the clients should wait between attempting acquisition and renewal of a leadership. This is only"+
+		"DEPRECATED(please pass configuration file via --config flag): The duration the clients should wait between attempting acquisition and renewal of a leadership. This is only"+
 			"applicable if leader election is enabled.")
 	flag.StringVar(&leaderElectResourceLock, "leader-elect-resource-lock", "leases",
-		"The type of resource object that is used for locking during leader election. Supported options are "+
+		"DEPRECATED(please pass configuration file via --config flag): The type of resource object that is used for locking during leader election. Supported options are "+
 			"'endpoints', 'configmaps', 'leases', 'endpointsleases' and 'configmapsleases'")
 	flag.StringVar(&leaderElectionID, "leader-elect-resource-name", "b8b2488c.x-k8s.io",
-		"The name of resource object that is used for locking during leader election. ")
+		"DEPRECATED(please pass configuration file via --config flag): The name of resource object that is used for locking during leader election. ")
 	flag.StringVar(&configFile, "config", "",
 		"The controller will load its initial configuration from this file. "+
 			"Command-line flags will override any configurations set in this file. "+
@@ -115,7 +114,7 @@ func main() {
 
 	ctrl.SetLogger(zap.New(zap.UseFlagOptions(&opts)))
 
-	options, cfg, err := apply(configFile, probeAddr, enableLeaderElection, leaderElectLeaseDuration, leaderElectRenewDeadline, leaderElectRetryPeriod, leaderElectResourceLock, leaderElectionID)
+	options, cfg, err := apply(configFile, probeAddr, enableLeaderElection, leaderElectLeaseDuration, leaderElectRenewDeadline, leaderElectRetryPeriod, leaderElectResourceLock, leaderElectionID, metricsAddr)
 	if err != nil {
 		setupLog.Error(err, "unable to load the configuration")
 		os.Exit(1)
@@ -132,35 +131,6 @@ func main() {
 		kubeConfig.Burst = burst
 	}
 
-	namespace := utils.GetOperatorNamespace()
-
-	// Disabling http/2 to prevent being vulnerable to the HTTP/2 Stream Cancellation and
-	// Rapid Reset CVEs. For more information see:
-	// - https://github.com/advisories/GHSA-qppj-fm5r-hxr3
-	// - https://github.com/advisories/GHSA-4374-p667-p6c8
-	disableHTTP2 := func(c *tls.Config) {
-		setupLog.Info("disabling http/2")
-		c.NextProtos = []string{"http/1.1"}
-	}
-
-	if !flagsSet["metrics-bind-address"] {
-		metricsAddr = cfg.Metrics.BindAddress
-	}
-
-	// Metrics endpoint is enabled in 'config/default/kustomization.yaml'. The Metrics options configure the server.
-	// More info:
-	// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.19.1/pkg/metrics/server
-	// - https://book.kubebuilder.io/reference/metrics.html
-	metricsServerOptions := metricsserver.Options{
-		BindAddress:    metricsAddr,
-		SecureServing:  true,
-		FilterProvider: filters.WithAuthenticationAndAuthorization,
-		TLSOpts:        []func(*tls.Config){disableHTTP2},
-	}
-
-	options.Metrics = metricsServerOptions
-	options.LeaderElectionNamespace = namespace
-
 	mgr, err := ctrl.NewManager(kubeConfig, options)
 	if err != nil {
 		setupLog.Error(err, "unable to start manager")
@@ -169,7 +139,7 @@ func main() {
 
 	certsReady := make(chan struct{})
 	if cfg.InternalCertManagement != nil && *cfg.InternalCertManagement.Enable {
-		if err = cert.CertsManager(mgr, namespace, *cfg.InternalCertManagement.WebhookServiceName, *cfg.InternalCertManagement.WebhookSecretName, cfg.Webhook.CertDir, certsReady); err != nil {
+		if err = cert.CertsManager(mgr, options.LeaderElectionNamespace, *cfg.InternalCertManagement.WebhookServiceName, *cfg.InternalCertManagement.WebhookSecretName, cfg.Webhook.CertDir, certsReady); err != nil {
 			setupLog.Error(err, "unable to setup cert rotation")
 			os.Exit(1)
 		}
@@ -247,7 +217,11 @@ func apply(configFile string,
 	leaderElectLeaseDuration time.Duration,
 	leaderElectRenewDeadline time.Duration,
 	leaderElectRetryPeriod time.Duration,
-	leaderElectResourceLock, leaderElectionID string) (ctrl.Options, configapi.Configuration, error) {
+	leaderElectResourceLock,
+	leaderElectionID string,
+	metricsAddr string) (ctrl.Options, configapi.Configuration, error) {
+	namespace := utils.GetOperatorNamespace()
+
 	options, cfg, err := config.Load(scheme, configFile)
 	if err != nil {
 		return options, cfg, err
@@ -278,6 +252,33 @@ func apply(configFile string,
 	if flagsSet["leader-elect-resource-name"] {
 		options.LeaderElectionID = leaderElectionID
 	}
+
+	// Disabling http/2 to prevent being vulnerable to the HTTP/2 Stream Cancellation and
+	// Rapid Reset CVEs. For more information see:
+	// - https://github.com/advisories/GHSA-qppj-fm5r-hxr3
+	// - https://github.com/advisories/GHSA-4374-p667-p6c8
+	disableHTTP2 := func(c *tls.Config) {
+		setupLog.Info("disabling http/2")
+		c.NextProtos = []string{"http/1.1"}
+	}
+
+	if !flagsSet["metrics-bind-address"] {
+		metricsAddr = cfg.Metrics.BindAddress
+	}
+
+	// Metrics endpoint is enabled in 'config/default/kustomization.yaml'. The Metrics options configure the server.
+	// More info:
+	// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.19.1/pkg/metrics/server
+	// - https://book.kubebuilder.io/reference/metrics.html
+	metricsServerOptions := metricsserver.Options{
+		BindAddress:    metricsAddr,
+		SecureServing:  true,
+		FilterProvider: filters.WithAuthenticationAndAuthorization,
+		TLSOpts:        []func(*tls.Config){disableHTTP2},
+	}
+
+	options.Metrics = metricsServerOptions
+	options.LeaderElectionNamespace = namespace
 
 	setupLog.Info("Successfully loaded configuration", "config", cfgStr)
 	return options, cfg, nil
