ea93dfbd5dd6f27415199c7b6312862b96f13952
Edwin Hernandez <Edandres249@gmail.com>
2025-03-17 09:26:18 -0700

    Update api/leaderworkerset/v1/leaderworkerset_types.go
    Co-authored-by: Kante Yin <kerthcet@gmail.com>

--- api/leaderworkerset/v1/leaderworkerset_types.go
+++ api/leaderworkerset/v1/leaderworkerset_types.go
@@ -212,7 +212,7 @@ const (
 	// If (LeaderWorkerSet.Spec.LeaderWorkerTemplate.Size-1) is divisible
 	// by LeaderWorkerSet.Spec.SubGroupPolicy.Size, the groups will look like:
 	// (0, 1, ... subGroupSize), (subGroupSize + 1, ... 2 * subGroupSize), ...
-	// If it is even, the groups will look like:
+	// If not divisible, the groups will look like:
 	// (0, 1, ... subGroupSize-1), (subGroupSize, ... 2*subGroupSize - 1), ...
 	SubGroupPolicyTypeLeaderWorker SubGroupPolicyType = "LeaderWorker"
 
