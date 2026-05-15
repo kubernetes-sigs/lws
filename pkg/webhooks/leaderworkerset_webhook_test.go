342df8bc4799f8cd2be787789ac161a1f81d4c72
liaolinrong <liaolinrong@126.com>
2026-05-15 19:35:57 +0800

    validate subgroup placement against the effective leader spec

--- pkg/webhooks/leaderworkerset_webhook_test.go
+++ pkg/webhooks/leaderworkerset_webhook_test.go
@@ -421,7 +421,21 @@ func TestValidateUpdateSubGroupPolicy(t *testing.T) {
 					v1.SubGroupPlacement{WorkerIndexes: []int32{3}, MatchLabels: map[string]string{"remote": "schedule_zone"}},
 				).
 				Obj(),
-			wantErr: "subGroupPlacement does not support leader requesting TPUs",
+			wantErr: "subGroupPlacement does not support TPU-requesting leader pods",
+		},
+		{
+			name: "subgroup placement rejects TPU-requesting effective leader when leader template is omitted",
+			lws: wrappers.BuildLeaderWorkerSet("default").
+				Size(4).
+				SubGroupType(leaderworkerset.SubGroupPolicyTypeLeaderExcluded).
+				LeaderTemplate(nil).
+				WorkerTemplateSpec(wrappers.MakeWorkerPodSpecWithTPUResource()).
+				SubGroupPlacement(
+					leaderworkerset.SubGroupPlacement{WorkerIndexes: []int32{1, 2}, MatchLabels: map[string]string{"local": "schedule_zone"}},
+					leaderworkerset.SubGroupPlacement{WorkerIndexes: []int32{3}, MatchLabels: map[string]string{"remote": "schedule_zone"}},
+				).
+				Obj(),
+			wantErr: "subGroupPlacement does not support TPU-requesting leader pods",
 		},
 		{
 			name: "valid subgroup placement",
