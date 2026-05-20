08e3c3a404beb59d42495a6c0335be7367a66e28
mostafa <mostafahass314@gmail.com>
2026-05-20 21:06:36 +0300

    feat(lws): improve feedback for malformed LWS resources

--- pkg/controllers/leaderworkerset_controller.go
+++ pkg/controllers/leaderworkerset_controller.go
@@ -75,6 +75,7 @@ const (
 	GroupsProgressing = "GroupsProgressing"
 	GroupsUpdating    = "GroupsUpdating"
 	CreatingRevision  = "CreatingRevision"
+	FailedUpdate      = "FailedUpdate"
 
 	// Event actions
 	Create = "Create"
@@ -165,7 +166,7 @@ func (r *LeaderWorkerSetReconciler) Reconcile(ctx context.Context, req ctrl.Requ
 		if leaderSts == nil {
 			r.Record.Eventf(lws, nil, corev1.EventTypeWarning, FailedCreate, Create, fmt.Sprintf("Failed to create leader statefulset %s: %v", lws.Name, err))
 		} else {
-			r.Record.Eventf(lws, nil, corev1.EventTypeWarning, FailedCreate, Update, fmt.Sprintf("Failed to update leader statefulset %s: %v", lws.Name, err))
+			r.Record.Eventf(lws, nil, corev1.EventTypeWarning, FailedUpdate, Update, fmt.Sprintf("Failed to update leader statefulset %s: %v", lws.Name, err))
 		}
 		return ctrl.Result{}, err
 	}
